60f6f5a15644c5633bfc1cabe94988463e63400a
Mateusz Kurzyna <lolc44t@gmail.com>
2026-01-02 16:27:32 +0100

    cz2

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,10 @@
 use std::io::{self, Write};
 
 fn main() {
-    // TODO: Uncomment the code below to pass the first stage
     print!("$ ");
     io::stdout().flush().unwrap();
     let mut cmd = String::new();
     io::stdin().read_line(&mut cmd).unwrap();
-    println!("{cmd}: command not found");
+    cmd = cmd.trim_end().trim_start().to_string();
+    println!("{cmd}: command not found")
 }
