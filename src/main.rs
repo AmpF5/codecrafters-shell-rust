f291288542d843dd9940176deace7a0328528f10
Mateusz Kurzyna <lolc44t@gmail.com>
2026-01-02 18:05:39 +0100

    pn5

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,25 @@
-#[allow(unused_imports)]
+mod command;
+mod commands;
+mod execute;
+
 use std::io::{self, Write};
+
+use crate::{command::Command, execute::Execute};
 
 fn main() {
     let mut input = String::new();
 
     loop {
         print!("$ ");
+
         io::stdout().flush().unwrap();
         input.clear();
         io::stdin()
             .read_line(&mut input)
             .expect("Cannot read command");
 
-        let cmd = input.trim();
+        let parsed_cmd = Command::match_cmd(input.trim());
 
-        println!("{cmd}: command not found")
+        parsed_cmd.execute();
     }
 }
