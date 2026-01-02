99441f9c442cb57f5b8e1c9b2f35a3de5a78fd52
Mateusz Kurzyna <lolc44t@gmail.com>
2026-01-02 16:30:38 +0100

    ff0

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,11 @@ fn main() {
     print!("$ ");
     io::stdout().flush().unwrap();
     let mut cmd = String::new();
-    io::stdin().read_line(&mut cmd).unwrap();
-    cmd = cmd.trim_end().trim_start().to_string();
-    println!("{cmd}: command not found")
+
+    loop {
+        cmd.clear();
+        io::stdin().read_line(&mut cmd).unwrap();
+        cmd = cmd.trim_end().trim_start().to_string();
+        println!("{cmd}: command not found")
+    }
 }
