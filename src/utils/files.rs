fea73ee414d998006a35d218f58f10a82f1a9691
Mateusz Kurzyna <lolc44t@gmail.com>
2026-01-19 21:19:22 +0100

    fix type cmd

--- src/utils/files.rs
+++ src/utils/files.rs
@@ -7,22 +7,15 @@ use std::{
 const KEY: &str = "PATH";
 
 pub fn find_exe_in_env(cmd: &str) -> Option<String> {
-    let path = find_path_in_env(cmd)?;
-    let metadata = fs::metadata(&path).ok()?;
-    if metadata.is_file() && metadata.permissions().mode() & 0o111 != 0 {
-        Some(path)
-    } else {
-        None
-    }
-}
-
-fn find_path_in_env(cmd: &str) -> Option<String> {
     let paths = env::var(KEY).ok()?;
 
     for path in env::split_paths(&paths) {
         let cmd_path = format!("{}/{}", path.display(), cmd);
 
-        if fs::metadata(&cmd_path).is_ok() {
+        if let Ok(metadata) = fs::metadata(&cmd_path)
+            && metadata.is_file()
+            && metadata.permissions().mode() & 0o111 != 0
+        {
             return Some(cmd_path);
         }
     }
