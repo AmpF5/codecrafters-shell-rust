7f499c739d063e9046e73a69d8a2709effe3caa5
Mateusz Kurzyna <lolc44t@gmail.com>
2026-01-20 21:18:32 +0100

    codecrafters submit [skip ci]

--- src/commands/cd.rs
+++ src/commands/cd.rs
@@ -1,7 +1,8 @@
-use std::env;
+use std::{env, path::PathBuf};
 
 pub fn execute(path: &str) {
-    let Some(path_type) = path.chars().next() else {
+    let mut path_chars = path.chars();
+    let Some(path_type) = path_chars.next() else {
         eprintln!("cannot get path");
         return;
     };
@@ -9,14 +10,35 @@ pub fn execute(path: &str) {
     let new_path = match path_type {
         '/' => {
             if crate::utils::files::is_dir_exists(path) {
-                path
+                PathBuf::from(path)
             } else {
                 eprintln!("cd: {path}: No such file or directory");
                 return;
             }
         }
-        _ => todo!(),
+        '~' => {
+            let Some(home_dir) = env::home_dir() else {
+                eprintln!("cannot get home dir");
+                return;
+            };
+            path_chars.next();
+            home_dir.join(path_chars.as_str())
+        }
+        _ => {
+            let Ok(current_dir) = env::current_dir() else {
+                eprintln!("cannot get current dir");
+                return;
+            };
+            current_dir.join(path)
+        }
     };
 
-    env::set_current_dir(new_path).expect("cannot change path to {path}");
+    if !&new_path.is_dir() {
+        eprintln!("cd: {path}: No such file or directory");
+        return;
+    }
+
+    if let Err(err) = env::set_current_dir(new_path) {
+        eprintln!("error: {}", err);
+    }
 }
