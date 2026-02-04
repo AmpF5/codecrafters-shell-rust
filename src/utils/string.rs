a90276d18f3085663b2635f923e562c1c310ec3c
Mateusz Kurzyna <lolc44t@gmail.com>
2026-02-04 19:24:14 +0100

    codecrafters submit [skip ci]

--- src/utils/string.rs
+++ src/utils/string.rs
@@ -4,42 +4,81 @@ const SPECIAL_CHARS: [char; 1] = ['\''];
 /// args as second value
 pub fn get_cmd_and_args(input: &str) -> (&str, Option<&str>) {
     let v = input.trim_end().splitn(2, " ").collect::<Vec<&str>>();
-    // println!("splitted: {:?}", v);
     (v[0], v.get(1).copied())
 }
 
 pub fn get_formatted_input(args: &str) -> Vec<String> {
     let mut r = Vec::new();
-    let mut word = String::new();
-    let mut s: Vec<char> = Vec::new();
+    let mut word_to_append = String::new();
+
+    let mut single_quotes = false;
+    let mut double_quotes = false;
 
     for ch in args.trim().chars() {
-        if SPECIAL_CHARS.contains(&ch) {
-            match s.last() {
-                Some(last_char) => {
-                    if *last_char == ch {
-                        s.pop();
-                    } else {
-                        s.push(ch);
-                    }
+        if ch == '\'' && !double_quotes {
+            single_quotes = !single_quotes;
+            continue;
+        }
+
+        if ch == '"' && !single_quotes {
+            double_quotes = !double_quotes;
+            continue;
+        }
+
+        if ch == ' ' {
+            if !(single_quotes || double_quotes) {
+                if word_to_append.is_empty() {
+                    continue;
                 }
-                None => s.push(ch),
-            }
-        } else if ch == ' ' {
-            if s.is_empty() {
-                if !word.is_empty() {
-                    r.push(word.clone());
-                    word.clear();
-                }
+
+                r.push(word_to_append.clone());
+                word_to_append.clear();
             } else {
-                word.push(ch);
+                word_to_append.push(ch);
             }
         } else {
-            word.push(ch);
+            word_to_append.push(ch);
         }
     }
 
-    r.push(word);
+    r.push(word_to_append);
 
     r
 }
+
+//
+// pub fn get_formatted_input(args: &str) -> Vec<String> {
+//     let mut r = Vec::new();
+//     let mut word = String::new();
+//     let mut s: Vec<char> = Vec::new();
+//
+//     for ch in args.trim().chars() {
+//         if SPECIAL_CHARS.contains(&ch) {
+//             match s.last() {
+//                 Some(last_char) => {
+//                     if *last_char == ch {
+//                         s.pop();
+//                     } else {
+//                         s.push(ch);
+//                     }
+//                 }
+//                 None => s.push(ch),
+//             }
+//         } else if ch == ' ' {
+//             if s.is_empty() {
+//                 if !word.is_empty() {
+//                     r.push(word.clone());
+//                     word.clear();
+//                 }
+//             } else {
+//                 word.push(ch);
+//             }
+//         } else {
+//             word.push(ch);
+//         }
+//     }
+//
+//     r.push(word);
+//
+//     r
+// }
